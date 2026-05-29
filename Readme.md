af83ff74f0e512ab4f0d25b088bd874215fb6af5
Daniel Fernandez Martinez <danifer1695@gmail.com>
2026-05-29 16:05:25 -0400

    updated default color scheme

--- Readme.md
+++ Readme.md
@@ -4,7 +4,7 @@ This extension will allow you to select any text containing Kanji to get some qu
 
 >This is intended as a portfolio project, and because of this its UI is intentionally made to resemble Migaku's fantastic visual identity.
 
-![Demo](assets/images/screenshots/screenshot_lookup_panel_02.png)
+![Demo](assets/images/screenshots/screenshot_lookup_panel_01.png)
 
 ## A little bit of background
 
--- shared/constants.js
+++ shared/constants.js
@@ -43,12 +43,12 @@ const PALETTES = {
         collection_button_01_text: "#fa6688",
 
         //------------JLPT Colors------------------
-        N5_FG: "#00c7a4", N5_BG: "#151314",
-        N4_FG: "#3c91ff", N4_BG: "#151314",
-        N3_FG: "#702bcb", N3_BG: "#151314",
-        N2_FG: "#fab73d", N2_BG: "#151314",
-        N1_FG: "#ff296d", N1_BG: "#151314",
-        NX_FG: "#9a9abd", NX_BG: "#151314",
+        N5_FG: "#00c7a4", N5_BG: "#15131470",
+        N4_FG: "#3c91ff", N4_BG: "#15131470",
+        N3_FG: "#8026f6", N3_BG: "#15131470",
+        N2_FG: "#fab73d", N2_BG: "#15131470",
+        N1_FG: "#ff296d", N1_BG: "#15131470",
+        NX_FG: "#9a9abd", NX_BG: "#15131470",
     },
 
 }; 
